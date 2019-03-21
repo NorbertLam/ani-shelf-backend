4992c03492433c1a895c4a96b55f18f05ae842f3
simk395 <simon.kong95@gmail.com>
2019-03-21 16:54:10 -0400

    on
    Signed-off-by: simk395 <simon.kong95@gmail.com>

--- db/migrate/20190321204329_create_animes.rb
+++ db/migrate/20190321204329_create_animes.rb
@@ -1,10 +1,10 @@
 class CreateAnimes < ActiveRecord::Migration[5.2]
   def change
     create_table :animes do |t|
-        t.string :name,
-        t.string :season,
-        t.string :info,
-        t.string :image,
+        t.string :name
+        t.string :season
+        t.string :info
+        t.string :image
         t.integer :year
         t.string :start_date
         t.string :end_date
