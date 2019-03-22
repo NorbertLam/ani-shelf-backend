877ab36e778257cc11049c8057f52a2b46fc9942
simk395 <simon.kong95@gmail.com>
2019-03-22 14:41:03 -0400

    on
    Signed-off-by: simk395 <simon.kong95@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require_relative '../data.rb'
 
-AnimeData.spring.each do |anime|
-    genres = anime[:genres].join(", ")
-    animeId = anime[:id]
-    malId = anime[:idMal]
-    title = anime[:title][:romaji]
-    desc = anime[:description]
-    coverImage = anime[:coverImage][:extraLarge]
-    startDate = "#{anime[:startDate][:month]} #{anime[:startDate][:day]}, #{anime[:startDate][:year]}"
-    trailerSite = "null"
-    trailerId = "null"
-    if anime[:trailer] != "null"
-        trailerSite = anime[:trailer][:site]
-        trailerId = anime[:trailer][:id]
-    end
-    season = anime[:season]
-    seasonInt = anime[:seasonInt]
-    year = anime[:startDate][:year]
-     Anime.create(anime_id:animeId, mal_id: malId, title: title, 
-        desc: desc , image:coverImage, genres: genres,
-        trailer_site: trailerSite, trailer_id: trailerId, start_date: startDate,
-        season: season, year: year)
+seasonArr = [AnimeData.summer, AnimeData.winter, AnimeData.spring, AnimeData.fall]
+seasonArr.each do |season|
+    season.each do |anime|
+        genres = anime[:genres].join(", ")
+        animeId = anime[:id]
+        malId = anime[:idMal]
+        title = anime[:title][:romaji]
+        desc = anime[:description]
+        coverImage = anime[:coverImage][:extraLarge]
+        startDate = "#{anime[:startDate][:month]} #{anime[:startDate][:day]}, #{anime[:startDate][:year]}"
+        trailerSite = "null"
+        trailerId = "null"
+        if anime[:trailer] != "null"
+            trailerSite = anime[:trailer][:site]
+            trailerId = anime[:trailer][:id]
+        end
+        season = anime[:season]
+        seasonInt = anime[:seasonInt]
+        year = anime[:startDate][:year]
+        Anime.create(anime_id:animeId, mal_id: malId, title: title, 
+            desc: desc , image:coverImage, genres: genres,
+            trailer_site: trailerSite, trailer_id: trailerId, start_date: startDate,
+            season: season, year: year)
+        end
 end
